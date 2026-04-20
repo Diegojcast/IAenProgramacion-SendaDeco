f6c1479702ec9064019643f3e5f7e59e640db04b
Diegojcast <55807246+Diegojcast@users.noreply.github.com>
2026-04-20 19:04:53 -0300

    agrego icono

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,21 +20,7 @@ export const metadata: Metadata = {
   description: 'Descubre nuestra colección de decoración artesanal hecha a mano. Macramé, cemento, velas y más.',
   generator: 'v0.app',
   icons: {
-    icon: [
-      {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
-      },
-    ],
-    apple: '/apple-icon.png',
+    icon: '/favicon.png',
   },
 }
 
