14a682a220a7731da9ab559995580b6579eb9093
Diegojcast <55807246+Diegojcast@users.noreply.github.com>
2026-04-20 13:42:06 -0300

    ERROR EN DEPLOY

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    images: {
+        unoptimized: true,
+    },
+    serverExternalPackages: ["@xenova/transformers", "sharp"],
 }
 
 export default nextConfig
